36779fe6e0f7f203e33d99b2a69e863b9e012a35
kmm2659377-spec <kmm2659377@stu.o-hara.ac.jp>
2026-05-13 10:54:26 +0900

    5/13水曜日　BGMの再生仕組み作成

--- Planet/Assets/Scripts/Kitano/Load/PuzzleManager.cs
+++ Planet/Assets/Scripts/Kitano/Load/PuzzleManager.cs
@@ -2,6 +2,12 @@ using UnityEngine;
 
 public class PuzzleManager : MonoBehaviour
 {
+    void Start()
+    {
+        Debug.Log("PuzzleManagerが起動しました。");
+        AudioManager.instance.PlayBGM("Puzzle");
+    }
+
     public void StartBattle()
     {
         Debug.Log("攻撃データ送信");
---
+++ Planet/Assets/Scripts/Kitano/Sound/AudioManeger.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    // Singleton
+    public static AudioManager instance;
+
+    // AudioSource
+    public AudioSource bgmSource;
+
+    public AudioSource seSource;
+
+    // BGM
+    public AudioClip puzzleBGM;
+
+    public AudioClip shootingBGM;
+
+    private string currentBGM = "";
+
+    void Awake()
+    {
+        // 重複防止
+        if (instance != null)
+        {
+            Destroy(gameObject);
+
+            return;
+        }
+
+        instance = this;
+
+        // シーン移動で破壊しない
+        DontDestroyOnLoad(gameObject);
+
+        Debug.Log("AudioManager 初期化");
+    }
+
+    public void PlayBGM(string bgmName)
+    {
+        // 同じBGMなら再生しない
+        if (currentBGM == bgmName)
+        {
+            return;
+        }
+
+        currentBGM = bgmName;
+
+        AudioClip clip = null;
+
+        // BGM選択
+        switch (bgmName)
+        {
+            case "Puzzle":
+                clip = puzzleBGM;
+                break;
+
+            case "Shooting":
+                clip = shootingBGM;
+                break;
+        }
+
+        // 再生
+        if (clip != null)
+        {
+            bgmSource.clip = clip;
+
+            bgmSource.Play();
+
+            Debug.Log("BGM再生 : " + bgmName);
+        }
+    }
+}
