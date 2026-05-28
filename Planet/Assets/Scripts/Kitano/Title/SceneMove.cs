e0ac128dd232a31e5abe4e8791550fce5f7bd3e9
ArukuHito0 <takeshita0205ayumu@gmail.com>
2026-05-28 14:29:53 +0900

    Revert "Merge branch 'main' into Ayumu_Takeshita"
    This reverts commit e900b7815fb07152302877e31de2d3ba6a30c535, reversing
    changes made to cda233d73a9842b4df0e50384d94143f01c15d6f.

--- Planet/Assets/Scripts/Kitano/Title/SceneMove.cs
+++ Planet/Assets/Scripts/Kitano/Title/SceneMove.cs
@@ -1,30 +1,10 @@
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class SceneMove : MonoBehaviour
 {
-    [Header("遷移先")]
-    public string nextSceneName = "Shooting Phase";
-
-    private Button button;
-    private RandomButtonDesign design;
-
-    void Start()
-    {
-        button = GetComponent<Button>();
-
-        design = GetComponent<RandomButtonDesign>();
-
-        button.onClick.AddListener(GoToGameScene);
-    }
-
     public void GoToGameScene()
     {
-        button.interactable = false;
-
-        SceneTransitionManager.instance.StartTransition(
-            nextSceneName,
-            design.selectedSprite
-        );
+        SceneManager.LoadScene("Shooting Phase");
     }
 }
